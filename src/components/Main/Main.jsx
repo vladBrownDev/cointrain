4a47639dd3dbd839e70c42c4668d2c62f3f4a225
vlad <dev.vladbrown@gmail.com>
2024-05-29 13:51:43 +0000

    (update) add chart logic

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,76 @@
 import './Main.scss';
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from 'react-router-dom';
+import Axios  from 'axios';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+export const options = {
+  responsive: true,
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+
+
 
 function Main() {
-  const { currency } = useParams();  
+  const [rates, setRates] = useState([])
 
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: '',
+      data: rates,
+      backgroundColor: [
+        '#F3BA2F',
+      ],
+      borderColor: [
+        '#F3BA2F',
+      ],
+      borderWidth: 3,
+      borderJoinStyle: 'miter',
+    }]
+  };
+
+  async function fetchChartData(priceArr = []) {
+    const result = await Axios.get('https://api.coinbase.com/v2/exchange-rates?currency=BTC');
+    priceArr.push(result.data.data.rates.USD)
+
+    if(priceArr.length >= 5) {
+      setRates(priceArr);
+      return;
+    };
+    
+    setTimeout(() => {fetchChartData(priceArr)}, 10000)
+  }
+
+  useEffect(() => {
+    fetchChartData()
+  }, [])
   return (
     <main>
-        {currency}
+      {rates[0] ? <Line options={options} data={data} /> : "loading..."}
     </main>
   );
 }
